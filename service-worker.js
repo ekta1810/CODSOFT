e9a19df12034ebfb325344b68cb8208250437d11
ekta1810 <ektasingh181004@gmail.com>
2025-08-10 17:24:51 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('quiz-app-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js',
-        './manifest.json',
-        './ico-123.png',
-        
-      ]);
+const CACHE_NAME = "quote-app-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icon.png",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
